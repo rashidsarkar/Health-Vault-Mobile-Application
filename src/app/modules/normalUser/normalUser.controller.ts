c0d290c30bc34b9f1203b33269af3b25ee6c11e9
rashidsarkar <rashidrock558@gmail.com>
2025-12-10 17:51:55 +0600

    images upload soho normal user create wiytth validation er kaj soro korbo...

--- src/app/modules/normalUser/normalUser.controller.ts
+++ src/app/modules/normalUser/normalUser.controller.ts
@@ -1,23 +1,23 @@
-import httpStatus from "http-status";
-import catchAsync from "../../utilities/catchasync";
-import sendResponse from "../../utilities/sendResponse";
-import normalUserServices from "./normalUser.service";
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import normalUserServices from './normalUser.service';
 
 const updateUserProfile = catchAsync(async (req, res) => {
-    const { files } = req;
-    if (files && typeof files === "object" && "profile_image" in files) {
-        req.body.profile_image = files["profile_image"][0].path;
-    }
-    const result = await normalUserServices.updateUserProfile(
-        req.user.profileId,
-        req.body
-    );
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: "Profile updated successfully",
-        data: result,
-    });
+  const { files } = req;
+  if (files && typeof files === 'object' && 'profile_image' in files) {
+    req.body.profile_image = files['profile_image'][0].path;
+  }
+  const result = await normalUserServices.updateUserProfile(
+    req.user.profileId,
+    req.body,
+  );
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Profile updated successfully',
+    data: result,
+  });
 });
 
 const NormalUserController = { updateUserProfile };
