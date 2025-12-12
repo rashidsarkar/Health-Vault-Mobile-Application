2500c8da647c52eea75f425400fd5d47aa84f2fe
rashidsarkar <rashidrock558@gmail.com>
2025-12-12 12:52:40 +0600

    `Refactor reminder controller and service to use http-status-codes and update reminder service to use req.user.profileID`

--- src/app/modules/reminder/reminder.controller.ts
+++ src/app/modules/reminder/reminder.controller.ts
@@ -1,28 +1,15 @@
-import httpStatus from 'http-status';
-import catchAsync from '../../utilities/catchasync';
-import sendResponse from '../../utilities/sendResponse';
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
 import reminderServices from './reminder.service';
 
-// const updateUserProfile = catchAsync(async (req, res) => {
-//   const { ...profileData } = req.body;
-//   const userId = req.params.userId;
-
-//   const updatedProfile = await reminderServices.updateUserProfile(
-//     userId,
-//     profileData
-//   );
-
-//   sendResponse(res, {
-//     statusCode: httpStatus.OK,
-//     success: true,
-//     message: "User profile updated successfully",
-//     data: updatedProfile,
-//   });
-// });
 const createReminder = catchAsync(async (req, res) => {
-  const result = await reminderServices.createReminder(req.body, req.profileID);
+  const result = await reminderServices.createReminder(
+    req.body,
+    req.user.profileID,
+  );
   sendResponse(res, {
-    statusCode: httpStatus.OK,
+    statusCode: StatusCodes.OK,
     success: true,
     message: 'Reminder created successfully',
     data: result,
--- src/app/modules/reminder/reminder.service.ts
+++ src/app/modules/reminder/reminder.service.ts
@@ -1,5 +1,3 @@
-import httpStatus from 'http-status';
-import AppError from '../../error/appError';
 import { IReminder } from './reminder.interface';
 import Reminder from './reminder.model';
 
