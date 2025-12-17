ec62bdcf96abc64c49cb8b7d00e085e1d611e2d4
rashidsarkar <rashidrock558@gmail.com>
2025-12-17 12:18:24 +0600

    `Refactor code in provider module`

--- src/app/modules/provider/provider.controller.ts
+++ src/app/modules/provider/provider.controller.ts
@@ -1,7 +1,17 @@
-import httpStatus from 'http-status';
-import catchAsync from '../../utilities/catchasync';
-import sendResponse from '../../utilities/sendResponse';
+import { StatusCodes } from 'http-status-codes';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
 import providerServices from './provider.service';
 
-const ProviderController = {};
+const getAllProviders = catchAsync(async (req, res) => {
+  const result = await providerServices.getAllProviders(req.query);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: 'Providers fetched successfully',
+    data: result,
+  });
+});
+
+const ProviderController = { getAllProviders };
 export default ProviderController;
--- src/app/modules/provider/provider.service.ts
+++ src/app/modules/provider/provider.service.ts
@@ -1,24 +1,36 @@
-import httpStatus from 'http-status';
-import AppError from '../../error/appError';
-import { IProvider } from './provider.interface';
 import Provider from './provider.model';
+import QueryBuilder from '../../builder/QueryBuilder';
 
-const updateUserProfile = async (id: string, payload: Partial<IProvider>) => {
-  if (payload.email || payload.username) {
-    throw new AppError(
-      httpStatus.BAD_REQUEST,
-      'You cannot change the email or username',
-    );
-  }
-  const user = await Provider.findById(id);
-  if (!user) {
-    throw new AppError(httpStatus.NOT_FOUND, 'Profile not found');
-  }
-  return await Provider.findByIdAndUpdate(id, payload, {
-    new: true,
-    runValidators: true,
-  });
+const getAllProviders = async (query: Record<string, unknown>) => {
+  const page = Number(query.page) || 1;
+  const limit = Number(query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const searchableFields = ['fullName', 'author', 'category'];
+
+  const queryBuilder = new QueryBuilder(Provider.find(), query)
+    .search(searchableFields)
+    .filter()
+    .sort();
+
+  // Count AFTER filters
+  const total = await Provider.countDocuments(
+    queryBuilder.modelQuery.getFilter(),
+  );
+
+  const result = await queryBuilder.modelQuery.skip(skip).limit(limit).exec();
+
+  const totalPage = Math.ceil(total / limit);
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+      totalPage,
+    },
+    data: result,
+  };
 };
-
-const ProviderServices = { updateUserProfile };
+const ProviderServices = { getAllProviders };
 export default ProviderServices;
