2105fa665772ece4080453cac420cd96008d5fa1
rashidsarkar <rashidrock558@gmail.com>
2025-12-26 17:12:20 +0600

    `Updated createInsurance function to use Mongoose ObjectId for normalUserId`

--- src/app/modules/insurance/insurance.service.ts
+++ src/app/modules/insurance/insurance.service.ts
@@ -1,9 +1,11 @@
 import { IInsurance } from './insurance.interface';
 import Insurance from './insurance.model';
+import { Types } from 'mongoose';
 
 const createInsurance = async (profileId: string, payload: IInsurance) => {
-  payload.normalUserId = profileId as any;
-  return await Insurance.create(payload);
+  payload.normalUserId = new Types.ObjectId(profileId);
+  const result = await Insurance.create(payload);
+  return result;
 };
 
 const InsuranceServices = { createInsurance };
